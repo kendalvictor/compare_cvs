75433e6d137ccc6da57bc255c387e55b242f1e1a
vvillacorta <v.villacorta.unmsm@gmail.com>
2017-02-20 04:21:57 -0500

    ADD limpiador de texto

--- apps/data/urls.py
+++ apps/data/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import home
+from .views import home, clean
 
 app_name = 'data'
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^clean/$', clean, name='clean'),
 ]
--- apps/data/views.py
+++ apps/data/views.py
@@ -1,9 +1,10 @@
-
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 from textblob import TextBlob as tb
 from .tfidf import tfidf
 from .models import Curriculum
+from .utils import clean_word
 
 
 def home(request):
@@ -19,5 +20,20 @@ def home(request):
             print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
 
     return render(request, 'home.html', locals())
+
+
+def clean(request):
+    curriculums = Curriculum.objects.all()[:5]
+    clean_list = [clean_word(curriculum.algoritmo_texto) for curriculum in curriculums]
+    for i, list in enumerate(clean_list):
+        print("Words in documents {0}::::::::::::::".format(i + 1))
+        print(list)
+
+    return render(request, 'clean.html', locals())
+
+
+
+
+
 
 
